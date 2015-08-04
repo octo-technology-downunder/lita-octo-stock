1552252031bf06be522cbe1f9c0f92161ecc9dc7
byalpel <david.alia@gmail.com>
2015-08-04 21:19:39 +1000

    Restricted to octo symbol

--- spec/lita/handlers/stocks_spec.rb
+++ spec/lita/handlers/stocks_spec.rb
@@ -1,33 +1,13 @@
 require "spec_helper"
 
 describe Lita::Handlers::Stocks, lita_handler: true do
-  it { routes_command("stock GOOG").to(:stock_info) }
-  it { doesnt_route_command("stock").to(:stock_info) }
+  it { routes_command("action octo").to(:stock_info) }
+  it { doesnt_route_command("action").to(:stock_info) }
 
-  it "checks default stock" do
-    send_command "stock GOOG"
-    expect(replies.last).to include("Google Inc")
-    expect(replies.last).to include("GOOG")
-    expect(replies.last).to include("NASDAQ")
+  it "checks general action octo" do
+    send_command "action octo"
+    expect(replies.last).to include("ALOCT")
+    expect(replies.last).to include("Octo Technology")
     expect(replies.last).to include("MktCap")
   end
-
-  it "checks an invalid stock" do
-    send_command "stock GOOGFAKE"
-    expect(replies.last).to eq("Sorry, but there was a problem retrieving stock information.")
-  end
-
-  it "checks a stock without mktcap" do
-    send_command "stock .DJI"
-    expect(replies.last).to include("Dow Jones Industrial")
-    expect(replies.last).to_not include("MktCap")
-    expect(replies.last).to_not include("P/E")
-  end
-
-
-  it "ensures proper encoding" do
-    send_command "stock T"
-    expect(replies.last).to include("AT&T")
-  end
-
 end
